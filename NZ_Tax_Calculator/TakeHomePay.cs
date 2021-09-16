031c8b645528035a99f8497a62dbb190c375dd28
Gyeongjae KIM <kkj96@hanmail.net>
2021-09-16 21:57:43 +1200

    Sprint 3 TakeHomePay Function added

---
+++ NZ_Tax_Calculator/TakeHomePay.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NZ_Tax_Calculator
+{
+    public class TakeHomePay
+    {
+        public void question()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Please choose one of belows for you.");
+            Console.WriteLine("1. Weekly Take Home Pay");
+            Console.WriteLine("2. Fortnightly Take Home Pay");
+            Console.WriteLine("3. Monthly TakeHomePay");
+
+            Answer();
+        }
+
+        public void Answer()
+        {
+            Weekly weekly = new Weekly();
+            Fortnightly fortnightly = new Fortnightly();
+            Monthly monthly = new Monthly();
+
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.D1:
+                    weekly.weekly_TakeHomePay();
+                    break;
+                case ConsoleKey.D2:
+                    fortnightly.fortnightly_TakeHomePay();
+                    break;
+                case ConsoleKey.D3:
+                    monthly.monthly_TakeHomePay();
+                    break;
+            }
+        }
+
+    }
+
+    public class Weekly
+    {
+        public void weekly_TakeHomePay()
+        {
+            double Take_Home_Pay;
+            Take_Home_Pay = (Tax_Generator.income - Tax_Generator.tax) / 52;
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine(" ___________________________________________");
+            Console.WriteLine("|                                           |");
+            Console.WriteLine("|            Here is your result            |");
+            Console.WriteLine("|                                           |");
+            Console.WriteLine("| Your annual income  is : NZD " + Tax_Generator.income);
+            Console.WriteLine("| Your Tax Payable is : NZD " + Tax_Generator.tax);
+            Console.WriteLine("| Your Weekly take home pay is : NZD " + Math.Round(Take_Home_Pay, 2));
+            Console.WriteLine("|___________________________________________|");
+
+            Console.ReadKey();
+            StartPage new_start = new StartPage();
+            new_start.InitializeWithQuestion();
+        }
+    }
+
+    public class Fortnightly
+    {
+        public void fortnightly_TakeHomePay()
+        {
+            double Take_Home_Pay;
+            Take_Home_Pay = (Tax_Generator.income - Tax_Generator.tax) / 26;
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine(" ___________________________________________");
+            Console.WriteLine("|                                           |");
+            Console.WriteLine("|            Here is your result            |");
+            Console.WriteLine("|                                           |");
+            Console.WriteLine("| Your annual income  is : NZD {0}", Tax_Generator.income);
+            Console.WriteLine("| Your Tax Payable is : NZD {0}", Tax_Generator.tax);
+            Console.WriteLine("| Your Fortnightly take home pay is : NZD {0} ", Math.Round(Take_Home_Pay, 2));
+            Console.WriteLine("|___________________________________________|");
+
+            Console.ReadKey();
+            StartPage new_start = new StartPage();
+            new_start.InitializeWithQuestion();
+        }
+    }
+
+
+    public class Monthly
+    {
+        public void monthly_TakeHomePay()
+        {
+            double Take_Home_Pay;
+            Take_Home_Pay = (Tax_Generator.income - Tax_Generator.tax) / 12;
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine(" ___________________________________________");
+            Console.WriteLine("|                                           |");
+            Console.WriteLine("|            Here is your result            |");
+            Console.WriteLine("|                                           |");
+            Console.WriteLine("| Your annual income  is : NZD " + Tax_Generator.income);
+            Console.WriteLine("| Your Tax Payable is : NZD " + Tax_Generator.tax);
+            Console.WriteLine("| Your Monthly take home pay is : NZD " + Math.Round(Take_Home_Pay, 2));
+            Console.WriteLine("|___________________________________________|");
+
+            Console.ReadKey();
+            StartPage new_start = new StartPage();
+            new_start.InitializeWithQuestion();
+        }
+
+    }
+}
--- NZ_Tax_Calculator/Tax_Generator.cs
+++ NZ_Tax_Calculator/Tax_Generator.cs
@@ -8,39 +8,46 @@ namespace NZ_Tax_Calculator
 {
     public class Tax_Generator
     {
-        public double tax;
+        public static double tax;
+        public static double income;
         public void Initialize_Tax_Generator()
         {
+            TakeHomePay takeHomePay = new TakeHomePay();
             Console.Clear();
             Console.WriteLine("Please enter your income: ");
-            double income = double.Parse(Console.ReadLine());
-            Console.WriteLine("Your Tax is {0}!!", Calculate(income));
+            income = double.Parse(Console.ReadLine());
+            Console.WriteLine("Your Tax is {0}!!", Calculate());
+            takeHomePay.question();
             Console.ReadKey();
 
         }
-        public double Calculate(double income)
+        public double Calculate()
         {
             if (income <= 14000)
             {
                 tax = income * 0.105;
-            }else if (14000 < income && income <= 48000)
+            }
+            else if (14000 < income && income <= 48000)
 
             {
                 tax = (income - 14000) * 0.175 + (14000 * 0.105);
-            }else if (48000 < income && income <= 70000)
+            }
+            else if (48000 < income && income <= 70000)
             {
                 tax = (income - 48000) * 0.3 + (34000 * 0.175) + (14000 * 0.105);
-            }else if (income > 70000)
+            }
+            else if (income > 70000)
             {
                 tax = (income - 70000) * 0.33 + (22000 * 0.3) + (34000 * 0.175) + (14000 * 0.105);
             }
-            return tax;        
-                    
-                    
-                    
-        }   
+            return tax;
+
+
+
+        }
 
 
     }
 
 }
+
